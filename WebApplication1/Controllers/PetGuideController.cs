28fc464c69528167a8bf3aab165645fba0275091
NgocDung2001 <dunglak2003@gmail.com>
2022-05-21 02:00:31 +0700

    first commit

---
+++ WebApplication1/Controllers/PetGuideController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PetGuideController : Controller
+    {
+        // GET: PetGuide
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
